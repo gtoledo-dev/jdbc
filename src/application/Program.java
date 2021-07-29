555c110d790e0dd8c602fa57db3548d7aa8a4d8d
gtoledo-dev <glauco_toledo@yahoo.com.br>
2021-07-28 21:09:51 -0300

    Deletar tabelas

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -20,14 +21,14 @@ public class Program {
 				+ "WHERE "
 				+ "Id = ?");
 		
-		
+		st.setInt(1, 9);
 		
 		int rowsAffected = st.executeUpdate();
 		
 		System.out.println("Done! Rows affected: " + rowsAffected);
 	}
 	catch (SQLException e) {
-		e.printStackTrace();
+		throw new DbIntegrityException(e.getMessage());
 	}
 	finally {
 		DB.closeStatement(st);
